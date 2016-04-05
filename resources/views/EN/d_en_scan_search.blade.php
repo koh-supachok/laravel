319a6ec6f848cf4377154a0ed4b63e2ff61d814a
koh-supachok <supachok_i@hotmail.com>
2016-04-05 08:02:52 +0700

    Edit EN_search

--- resources/views/EN/d_en_scan_search.blade.php
+++ resources/views/EN/d_en_scan_search.blade.php
@@ -12,89 +12,63 @@
             <div class="col-lg-12">
 
                 <div class="ibox float-e-margins">
-
                     <div class="ibox-title">
                         <h5>Chosen select <small>http://harvesthq.github.io/chosen/</small></h5>
                         <div class="ibox-tools">
                             <a class="collapse-link">
                                 <i class="fa fa-chevron-up"></i>
                             </a>
-                            <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                                <i class="fa fa-wrench"></i>
-                            </a>
-                            <ul class="dropdown-menu dropdown-user">
-                                <li><a href="#">Config option 1</a>
-                                </li>
-                                <li><a href="#">Config option 2</a>
-                                </li>
-                            </ul>
                             <a class="close-link">
                                 <i class="fa fa-times"></i>
                             </a>
                         </div>
                     </div>
                     <div class="ibox-content">
-                        <div class="ibox-content">
-                            <form method="get" class="form-horizontal">
-                                <div class="form-group"><label class="col-sm-2 control-label">Column sizing</label>
-                                    <div class="col-sm-10">
-                                        <div class="row">
-                                            <div class="col-md-4"><input type="text" id="search_item" placeholder="รหัสแฟ้ม" class="form-control"></div>
-                                        </div>
+                        <div class="row">
+                            <div class="col-lg-4">
+                                <div class="panel panel-default">
+                                    <div class="panel-heading">
+                                        รหัสแฟ้ม
+                                    </div>
+                                    <div class="panel-body">
+                                        <input type="text" id="search_item" placeholder="รหัสแฟ้ม" class="form-control">
                                     </div>
                                 </div>
-                            </form>
-                        </div>
-                        <p>
-                            With chosen select uesr can fase chose from large in select input.
-                        </p>
-                        <div class="form-group">
-                            <label class="font-noraml">Basic example</label>
-                            <div class="input-group">
-                                <select data-placeholder="Choose a Country..." class="chosen-select" style="width:350px;" tabindex="2">
-                                    <option value="">Select</option>
-                                    <option value="United States">United States</option>
-                                    <option value="United Kingdom">United Kingdom</option>
-                                    <option value="Afghanistan">Afghanistan</option>
-                                    <option value="Aland Islands">Aland Islands</option>
-                                    <option value="Albania">Albania</option>
-                                    <option value="Algeria">Algeria</option>
-                                    <option value="American Samoa">American Samoa</option>
-                                    <option value="Andorra">Andorra</option>
-                                    <option value="Angola">Angola</option>
-                                    <option value="Anguilla">Anguilla</option>
-                                    <option value="Antarctica">Antarctica</option>
-                                    <option value="Antigua and Barbuda">Antigua and Barbuda</option>
-                                    <option value="Argentina">Argentina</option>
-                                </select>
                             </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="font-noraml">Multi select</label>
-                            <div class="input-group">
-                                <select data-placeholder="Choose a Country..." class="chosen-select" multiple style="width:350px;" tabindex="4">
-                                    <option value="">Select</option>
-                                    <option value="United States">United States</option>
-                                    <option value="United Kingdom">United Kingdom</option>
-                                    <option value="Afghanistan">Afghanistan</option>
-                                    <option value="Aland Islands">Aland Islands</option>
-                                    <option value="Albania">Albania</option>
-                                    <option value="Algeria">Algeria</option>
-                                    <option value="American Samoa">American Samoa</option>
-                                    <option value="Guinea">Guinea</option>
-                                    <option value="Guinea-bissau">Guinea-bissau</option>
-                                    <option value="Guyana">Guyana</option>
-                                    <option value="Haiti">Haiti</option>
-                                    <option value="Heard Island and Mcdonald Islands">Heard Island and Mcdonald Islands</option>
-                                    <option value="Holy See (Vatican City State)">Holy See (Vatican City State)</option>
-                                    <option value="Honduras">Honduras</option>
-                                    <option value="Hong Kong">Hong Kong</option>
-                                    <option value="Hungary">Hungary</option>
-                                    <option value="Iceland">Iceland</option>
-                                    <option selected value="India">India</option>
-                                    <option value="Indonesia">Indonesia</option>
-                                    <option value="Iran, Islamic Republic of">Iran, Islamic Republic of</option>
-                                </select>
+                            <div class="col-lg-4">
+                                <div class="panel panel-primary">
+                                    <div class="panel-heading">
+                                        หมายเลขแบบ
+                                    </div>
+                                    <div class="panel-body">
+                                        <input type="text" id="search_drawing" placeholder="หมายเลขแบบ" class="form-control">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-4">
+                                <div class="panel panel-success">
+                                    <div class="panel-heading">
+                                        ประเภท
+                                    </div>
+                                    <div class="panel-body">
+                                        <select data-placeholder="Choose a Country..." class="chosen-select"  tabindex="2">
+                                            <option value="">Select</option>
+                                            <option value="United States">United States</option>
+                                            <option value="United Kingdom">United Kingdom</option>
+                                            <option value="Afghanistan">Afghanistan</option>
+                                            <option value="Aland Islands">Aland Islands</option>
+                                            <option value="Albania">Albania</option>
+                                            <option value="Algeria">Algeria</option>
+                                            <option value="American Samoa">American Samoa</option>
+                                            <option value="Andorra">Andorra</option>
+                                            <option value="Angola">Angola</option>
+                                            <option value="Anguilla">Anguilla</option>
+                                            <option value="Antarctica">Antarctica</option>
+                                            <option value="Antigua and Barbuda">Antigua and Barbuda</option>
+                                            <option value="Argentina">Argentina</option>
+                                        </select>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
